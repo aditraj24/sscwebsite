813de03213644324f44a81909bade2992c5e6126
aditraj24 <adityarajykme24@gmail.com>
2025-07-10 01:23:02 +0530

    added metadata

--- webssc/app/layout.js
+++ webssc/app/layout.js
@@ -2,25 +2,22 @@ import Navbar from "./navbar";
 import Footer from "./footer";
 import "./globals.css";
 
-// export default function RootLayout({ children }) {
-//   return (
-//     <html>
-//       <body className="min-h-screen flex flex-col">
-//         <Navbar />
-//         <main className="flex-grow pt-16">{children}</main>
-//         <Footer />
-//       </body>
-//     </html>
-//   );
-// }
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className="min-h-screen flex flex-col">
-        <Navbar />
-        <main className="flex-grow pt-16">{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <>
+      <head>
+        <title>SSC NIT Jamshedpur</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="ssclogo.png" type="image/x-icon"></link>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+      </head>
+      <html lang="en">
+        <body className="min-h-screen flex flex-col">
+          <Navbar />
+          <main className="flex-grow pt-16">{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </>
   );
 }
